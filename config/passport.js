6214ba722871f6f039a44e1ab06d1660bc607b10
David Scott Perez <david.scott.perez@live.com>
2018-07-24 22:56:14 -0700

    submitted

--- config/passport.js
+++ config/passport.js
@@ -34,34 +34,39 @@ passport.use(
         });
     })
     );
+// passport.use(
+//     'local',
+//     new LocalStrategy(function(username, admin, done) {
+//         models.users
+//         .findOne({
+//             where: {
+//                 Username: username, 
+//                 Admin: true
+//             }
+//         }).then(user => {
+//             if (!user) {
+//                 console.log('not admin');
+//                 return done(null, false, {
+//                     message: 'you are not admin and do not have access to this part of the app'
+//                 });
+//             }
+//             if (user.Admin !== admin) {
+//                 console.log('no access');
+//                 return done(null, false, {
+//                     message: 'you do not have access to this part of the page'
+//                 });
+//             }
+//             return done(null, user);
+//         })
+//         .catch(err => {
+//             if (err) {
+//                 console.log('oops');
+//                 return done(err);
+//             }
+//         });
+//     })
+// );
 
-    passport.use(
-        'local',
-        new LocalStrategy(function(username, password, done) {
-            models.users
-            .findOne({
-                where: {
-                    Admin: true
-                }
-            })
-            .then(user => {
-                if (!user) {
-                    console.log('not a user');
-                    return done(null, false, {
-                        message: 'Incorrect Username'
-                    });
-                }
-            
-            return done(null, user);
-            })
-            .catch(err => {
-                if (err) {
-                    console.log('error');
-                    return done(err);
-                }
-            });
-        })
-        );
 
     passport.serializeUser((user, cb) => {
         cb(null, user.UserId);
